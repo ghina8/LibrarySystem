1be7f3fc3ebf56fee49e929bdbd59b57ece3fe99
Ghina Tayba <ghinatayba@gmail.com>
2026-05-30 23:53:53 +0300

    Created Users API Controller

---
+++ LibraryAPI/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using LibraryAPI.Data;
+using LibraryAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            return Ok(_context.Users.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            return Ok(user);
+        }
+    }
+}

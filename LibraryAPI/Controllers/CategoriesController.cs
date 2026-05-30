567cc574e7d9e0228156422aa609e48f8d5781ca
Ghina Tayba <ghinatayba@gmail.com>
2026-05-30 23:51:15 +0300

    Created Categories API Controller

---
+++ LibraryAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using LibraryAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetCategories()
+        {
+            return Ok(_context.Categories.ToList());
+        }
+    }
+}
